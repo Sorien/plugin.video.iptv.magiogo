cc3203a5d86f94dc6a9d3c69e6d49f0c685b653e
Sorien <sorien@mail.com>
2020-01-13 17:57:32 +0100

    1.0.2

--- magio/magiogo.py
+++ magio/magiogo.py
@@ -9,7 +9,7 @@ except:
     pass
 
 from builtins import super
-from iptv.client import IPTVClient, UserNotDefinedException, UA, Channel, StreamInfo, Programme, UserInvalidException, dummy_progress
+from iptv.client import IPTVClient, UserNotDefinedException, Channel, StreamInfo, Programme, UserInvalidException, dummy_progress
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
 
